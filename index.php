1290b2be0efb64e0587fc8cfb58c45d076df65e1
rghd-sudo <rghd4056@gmail.com>
2025-09-14 02:41:12 +0300

    Db with php is good

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$dbname = "athar_graduate";
+$dbname = "agdb";
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully!!!";
 ?>
