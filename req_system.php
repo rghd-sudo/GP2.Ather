b45c54b1ac6e9299f340d3f1dc26ca4ff77e86ea
rghd-sudo <rghd4056@gmail.com>
2025-11-13 02:46:54 +0300

    0

--- req_system.php
+++ req_system.php
@@ -44,7 +44,7 @@ $result = $conn->query($sql);
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-
+    
 <style>
 /* 🔹 General Layout */
 body {
